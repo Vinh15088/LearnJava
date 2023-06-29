8b331bffafc0d8e810926c79aba51a9861f8a7d5
VinhSeo <vinhzz1508@gmail.com>
2023-06-29 11:50:02 +0700

    Second commit

---
+++ src/Bai6_EpKieuDuLieu.java
@@ -0,0 +1,19 @@
+public class Bai6_EpKieuDuLieu {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        double kq = (double) a/b;
+        System.out.println(kq);
+
+        // Ep kieu hep
+        int c = 128;
+        byte d = (byte) c;
+        System.out.println(c);
+        System.out.println(d);
+
+        int e = 15;
+        byte f = (byte) e;
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
